5e827f1d3aaa631a3df02d351f91bcc990981f32
Jay Jahanzad <jahanzad@Jays-MacBook-Pro.local>
2023-11-24 18:28:25 -0500

    corrected API call to dictionary + added comments

--- Wordly/ContentView.swift
+++ Wordly/ContentView.swift
@@ -10,44 +10,67 @@ import Foundation
 
 struct ContentView: View {
     
+    // State variable to hold the fetched dictionary results
     @State private var results = [DictionaryWord]()
     
     var body: some View {
         VStack {
-            Text("Wordly - Get a new word daily!")
+            VStack{
+                // Application title and description
+                Text("Wordly")
+                    .fontDesign(.rounded)
+                    .font(.title)
+                Text("Get a new word everyday!")
+            }
+            .foregroundColor(.purple)
+            .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+            
+            // List view to display dictionary results
             List(results) { item in
                 VStack(alignment: .leading) {
                     Text(item.word).font(.headline)
-                        .foregroundColor(.blue)
+                        .foregroundColor(.purple)
+                    
+                    // Display phonetics information
                     ForEach(item.phonetics) { phone in
                         if phone.text != nil && phone.sourceURL != nil {
                             Text(phone.text!)
-                            Text(phone.sourceURL!)
+//                            Text(phone.sourceURL!)
                         }
                     }
+                    // Display meanings information
                     ForEach(item.meanings) { means in
+                        ForEach(means.definitions) { def in
+                            Text(def.definition)
+                        }
+                        // Display part of speech if available
                         if means.partOfSpeech != nil {
-                            Text(means.partOfSpeech!)
+                            Text(means.partOfSpeech)
                         }
                     }
                 }
             }
         }
         .task {
+            // Fetch data when the view is loaded
             await loadData()
         }
     }
     
-    let freeDictionaryURL = "https://api.dictionaryapi.dev/api/v2/entries/en/funny"
+    // Default word of the day testing purposes
+    let wordOfDay = "funny"
+    // API URL for fetching dictionary data
+    var freeDictionaryURL = "https://api.dictionaryapi.dev/api/v2/entries/en/"
     
+    // Function to fetch data from the dictionary API
     func loadData() async {
-        guard let url = URL(string: freeDictionaryURL) else {
-            print("Invalid URL")
-            return
-        }
+        let url = URL(string: "\(freeDictionaryURL)\(wordOfDay)")!
         do {
+            // Fetch data from the API
             let (data, _) = try await URLSession.shared.data(from: url)
+            // Decode the JSON data into an array of DictionaryWord objects
             let decoded: [DictionaryWord] = try JSONDecoder().decode([DictionaryWord].self, from: data)
+            // Update the results state variable
             results = decoded
         } catch {
             print(error)
@@ -62,6 +85,7 @@ struct ContentView_Previews: PreviewProvider {
     }
 }
 
+// Model structure for a dictionary word
 struct DictionaryWord: Identifiable, Codable {    // <--- here
     let id = UUID()
     
